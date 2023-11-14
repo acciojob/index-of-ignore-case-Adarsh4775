3650a7f3c423658019717910df2bf5cb1fb91216
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-14 17:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+	let str = s1.toLowerCase();
+	let subStr = s2.toLowerCase();
+	
+	for(i=0; i<str.length; i++)
+		{
+			let currSubStr = str.slice(i, i + subStr.length);
+
+			if(currSubStr === subStr)
+			{
+				return i;  
+			}
+		}
+	return -1;
 }
 
 // Please do not change the code below
